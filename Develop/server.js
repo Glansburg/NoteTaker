dfd503c1d7191d0ff17e86668913d47c7e3fe8e2
Joel Getzke <joelcode2023@gmail.com>
2023-07-23 22:35:09 -0500

    NOTE TAKER

--- Develop/server.js
+++ Develop/server.js
@@ -4,17 +4,12 @@ const app = express();
 const path = require("path");
 const fs = require("fs");
 const PORT = 3001;
-// const routes = require("./routes"); 
-// when it's ./routes, it looks for either routes.js or routes/index.js
-// const routes = require("./routes/index.js"); 
+
 
 app.use(express.json())
-// fetch... body:{...}
+
 app.use(express.urlencoded({ extended: false }));
-// <form> <label for="name">Name:</label> 
-// <input type="text" id="name" name="customer.name">
-// <label for="phonenumber">Phone:</label> 
-// <input type="text" id="phonenumber" name="customer.phonenumber">
+
 
 app.use(express.static(path.join(__dirname, "./public")))
 
@@ -28,7 +23,7 @@ app.get("/notes", (req, res) => {
     // fetch... then... then... response.players in a for loop
 })
 
-///api/note
+
 app.get("/api/notes", (req, res) => {
     let fileText = fs.readFileSync(path.join(__dirname, "./db/db.json"), "utf8", (err) => {
         if (err) throw err;
@@ -45,25 +40,20 @@ app.post("/api/notes", (req, res) => {
         })
     let filePostArray = JSON.parse(filePost);
     filePostArray.push(req.body);
+    //overwritting db.json
     fs.writeFileSync(path.join(__dirname, "./db/db.json"), JSON.stringify(filePostArray), "utf8",
         (err) => {
             if (err) throw err;
         })
 
     res.json({})
+    
 
-    // Read the notes into an array like before
-    // Push `req.body` into the array
-    /*
-        original array arrayOfNotes [ { title: 'Test Title', text: 'Test text' } ]
-        arrayOfNote.push(req.body)
-        req.body { title: 'a', text: 'b' }
 
-        Such that:
-        original array arrayOfNotes [ { title: 'Test Title', text: 'Test text' },  { title: 'a', text: 'b' } ]
+  
 
-    */
-    // Then rewrite / overwrite the db.json
+    
+  
     console.log(req.body)
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
